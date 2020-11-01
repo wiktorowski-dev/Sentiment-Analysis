053519c09413ad3fba725acefe36b1619527006b
Kamil <kamekw@gmail.com>
2020-11-01 21:10:17 +0100

    .py file implemented

---
+++ functions/functions.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import json
+import os
+import re
+
+
+# Slice large file to plenty of smaller ones
+
+def slicer(input_path_to_file, output_file_path, output_lines):
+    out = []
+    i = 0
+    with open(input_path_to_file) as file:
+        for line in file:
+            out.append(line)
+            if len(out) > output_lines:
+                print(i)
+                with open(output_file_path.format(i), mode='a') as file2:
+                    file2.write(json.dumps(out))
+                    i += 1
+                    out = []
+        if len(out) != 0:
+            with open(output_file_path.format(i), mode='a') as file2:
+                file2.write(json.dumps(out))
+
+
+def json_to_csv(file_name):
+    print('\t{}'.format(file_name))
+    # Process data
+    # Please notice that we using same name of variable to decrease memory usage
+
+    with open(file_name) as f:
+        file = json.load(f)
+
+    # Unpack json rows
+    file = [json.loads(x) for x in file]
+
+    file = pd.DataFrame(file)
+
+    # Remove unnecessary columns
+    file = file.filter(['overall', 'reviewText'])
+
+    # Rename columns
+    file = file.rename(columns={'overall': 'sentiment', 'reviewText': 'text'})
+
+    # Transform 0-5 score, into positive/negative values
+
+    file = file[file.sentiment != 3]
+    file.sentiment = file.sentiment.apply(lambda x: 'positive' if x > 3 else 'negative')
+
+    # Remove noises
+    # Make all to string
+    file['text'] = file['text'].apply(str)
+
+    # We can remove some duplicates also at this point
+    file = file.drop_duplicates()
+
+    patterns = [
+        r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)",
+        r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)",
+        r"(@[A-Za-z0-9_]+)",
+        r"(#[A-Za-z0-9_]+)"]
+
+    for pattern in patterns:
+        file['text'] = file['text'].apply(lambda x: re.sub(pattern, '', str(x)))
+
+    file.text = file.text.apply(lambda x: x if 0 < len(str(x).split()) < 100 else None)
+
+    # Remove nones
+    file = file.dropna()
+
+    # Write into csv file
+    file.to_csv(file_name, mode='w', index=False)
+
+
+def duplicate_remover(input_file_name,
+                      output_file_name_neg,
+                      output_file_name_pos,
+                      files_in_folder,
+                      auto_remove_files=False):
+    # Drop duplicates
+    list_files_1 = [x for x in range(files_in_folder)]
+    list_files_2 = [x for x in range(files_in_folder)]
+
+    for i in list_files_1:
+        print('duplicate_remover {}'.format(i))
+        list_files_2.remove(i)
+        df1 = pd.read_csv(input_file_name.format(i))
+        # Remove duplicated rows in data frame
+        df1 = df1.drop_duplicates()
+
+        # Prepare positive list of sentences and filter them
+        pos1 = set(df1[df1.sentiment == 'positive']['text'])
+        pos1 = set(filter(lambda x: type(x) == str, pos1))
+
+        # Prepare negative list of sentences and filter them
+        neg1 = set(df1[df1.sentiment == 'negative']['text'])
+        neg1 = set(filter(lambda x: type(x) == str, neg1))
+
+        for i2 in list_files_2:
+            print('\t{}'.format(i2))
+            df2 = pd.read_csv(input_file_name.format(i2))
+            df2 = df2.drop_duplicates()
+            pos2 = set(df2[df2.sentiment == 'positive']['text'])
+            neg2 = set(df2[df2.sentiment == 'negative']['text'])
+
+            # Remove duplicates sentences
+            pos1 = pos1 - pos2
+            neg1 = neg1 - neg2
+
+        # If you would like to don't use separate files use this lines below
+        # pos1 = [{'sentiment': 'positive', 'text': x} for x in pos1]
+        # neg1 = [{'sentiment': 'negative', 'text': x} for x in neg1]
+        # df1 = pd.DataFrame(pos1 + neg1)
+
+        df1 = pd.DataFrame(list(pos1))
+        df2 = pd.DataFrame(list(neg1))
+        df1.to_csv(output_file_name_pos.format(i), mode='a', index=False)
+        df2.to_csv(output_file_name_neg.format(i), mode='a', index=False)
+        if auto_remove_files:
+            os.remove(input_file_name.format(i))
+
+
+def count_files_in_dir(input_file_path_with_name):
+    counted_files_in_folder = [x for x in os.listdir(os.path.split(input_file_path_with_name)[0]) if
+                               os.path.split(input_file_path_with_name)[-1].replace("{", '').replace("}", '') in x
+                               and os.path.split(input_file_path_with_name)[-1].replace("{", '').replace("}", '') ==
+                               re.sub(r'\d', '', x)].__len__()
+
+    return counted_files_in_folder
+
+
+def count_rows_in_files(input_file, files: int):
+    out = []
+    out_sum = 0
+    for i in range(files):
+        df = pd.read_csv(input_file.format(i))
+        out.append(df.__len__())
+    for elem in out:
+        out_sum += elem
+    return out_sum
---
+++ sentiment_analysis.py
@@ -0,0 +1,68 @@
+from re_sizer import ReSizerFiles
+from functions import *
+import multiprocessing
+import time
+
+
+if __name__ == '__main__':
+    # Declare re sizer function
+    re_sizer = ReSizerFiles.re_sizer
+    #
+    tt = time.time()
+    #
+
+    # Slicer variables
+    input_path_to_large_file = r'D:\s_a\All_Amazon_Review.json'
+    output_file_path_post_slicer = r'D:\s_a\file_{}'
+
+    # .json to .csv formatter
+    cores = 4
+    input_file_path = r'D:\s_a\file_{}'
+
+    # Duplication remover variables
+    input_file_name = r'D:\s_a\file_large_{}'
+    output_file_name_neg = r'D:\s_a\file_large_n_{}'
+    output_file_name_pos = r'D:\s_a\file_large_p_{}'
+
+    # Re sizer variables
+    output_positive_files = r'D:\s_a\p_{}'
+    output_negative_files = r'D:\s_a\n_{}'
+
+    slicer(input_path_to_large_file, output_file_path_post_slicer, 1200000)
+
+    files_name = [input_file_path.format(x) for x in range(count_files_in_dir(input_file_path))]
+    with multiprocessing.Pool(cores) as p:
+        p.map(json_to_csv, files_name)
+
+    re_sizer(input_file_name=input_file_path,
+             output_file_name=input_file_name,
+             max_length_of_single_output=8000000,
+             auto_file_remover=True,
+             auto_detect_files_in_dir=True,
+             files_in_folder=None)
+
+    duplicate_remover(input_file_name,
+                      output_file_name_neg,
+                      output_file_name_pos,
+                      files_in_folder=count_files_in_dir(input_file_name),
+                      auto_remove_files=True)
+
+    re_sizer(input_file_name=output_file_name_neg,
+             output_file_name=output_negative_files,
+             max_length_of_single_output=1000000,
+             auto_file_remover=True,
+             auto_detect_files_in_dir=True,
+             files_in_folder=None)
+
+    re_sizer(input_file_name=output_file_name_pos,
+             output_file_name=output_positive_files,
+             max_length_of_single_output=1000000,
+             max_amount_output_of_files=count_files_in_dir(output_negative_files),
+             total_length_of_output=count_rows_in_files(output_negative_files,
+                                                        count_files_in_dir(output_negative_files)),
+             auto_file_remover=True,
+             auto_detect_files_in_dir=True,
+             files_in_folder=None)
+
+    o = time.time() - tt
+    print(o)
